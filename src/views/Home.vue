16df951846ecfc0696d2c0ac0a3f2ef87e208712
IoTKETI <iot.keti@gmail.com>
2023-02-08 14:55:15 +0900

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+
+ <v-container>
+    <ant/>
+  </v-container>
+
+</template>
+
+<script>
+//import CreateRS from '../components/createRS.vue'
+import ant from '../components/ant.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+    //CreateRS,
+    ant,
+},
+  }
+</script>
